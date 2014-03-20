9c24e4bafca5d1859d1c601d275cc87393cc5c49
jcdavison <jcdavison@vm-7.jcdavison.koding.kd.io>
2014-03-20 21:34:38 +0000

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HackerNewsClone::Application.config.secret_key_base = '77e8603e6203f7fe5c39b5a89ab1da3f9baa6b86f2c666e8edf78145cf482d4751912d32e38ae0640a0ac2462ea5b030a43e78f010137ebdc3305907079cc602'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HackerNewsClone::Application.config.session_store :cookie_store, key: '_hacker-news-clone_session'
